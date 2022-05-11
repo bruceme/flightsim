b08de250a3078544574da4f6c4849884bdc09263
William Engman <william.engman@hotmail.com>
2022-05-11 13:23:16 +0200

    merge resolve

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,10 @@ mod asset_manager;
 mod entity;
 mod helper;
 mod input_handler;
-<<<<<<< HEAD
 mod mesh_factory;
-=======
 mod model;
 mod window_handler;
 mod world;
->>>>>>> 928e71403e5375d67113a0104c37baff017bbb85
 
 fn main() {
     WindowHandler::new(1600, 900, false).run();
