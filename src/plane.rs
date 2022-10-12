393a8180f64678ed830d60aa5fd789c398f8c947
BruceMe <meabru@gmail.com>
2022-10-12 00:28:10 -0500

    Added Text Rendering

--- src/plane.rs
+++ src/plane.rs
@@ -1,11 +1,9 @@
-use cgmath::{Deg, EuclideanSpace, InnerSpace, Matrix4, Point3, Rad, Vector3, Vector4};
+use cgmath::{Deg, EuclideanSpace, InnerSpace, Matrix4, Point3, Vector3, Vector4};
+use glow_glyph::{GlyphBrushBuilder, ab_glyph, Section, Text};
 
-use crate::{camera::Camera, input_handler::KeyState, model::Model, window_handler::GlContext};
+use crate::{camera::Camera, input_handler::KeyState, window_handler::GlContext};
 
 pub struct Plane {
-    body: Model,
-    propeller: Model,
-
     scale: f32,
     position: Vector3<f32>,
 
@@ -18,7 +16,6 @@ pub struct Plane {
 
     velocity: Vector3<f32>,
 
-    propeller_rotation: f32,
     camera_offset: Vector3<f32>,
 }
 
@@ -29,11 +26,8 @@ impl Plane {
     pub const DRAG: f32 = 0.003;
     pub const LIFT: f32 = 0.0005;
 
-    pub fn new(body: Model, propeller: Model, position: Vector3<f32>) -> Self {
+    pub fn new(position: Vector3<f32>) -> Self {
         Self {
-            body,
-            propeller,
-
             scale: 0.25,
             position,
 
@@ -45,7 +39,6 @@ impl Plane {
             roll_velocity: 0.0,
 
             velocity: Vector3::new(0.0, 0.0, 1.0),
-            propeller_rotation: 0.0,
             camera_offset: Vector3::new(0.0, 3.0, -12.0),
         }
     }
@@ -115,7 +108,7 @@ impl Plane {
         //println!("{}", self.velocity.magnitude());
     }
 
-    pub fn render(&mut self, gl: &GlContext, time: &f32, camera: &mut Camera) {
+    pub fn render(&mut self, gl: &GlContext, camera: &mut Camera) {
         camera.perspective.fovy = Deg((70.0 + self.velocity.magnitude() * 40.0).clamp(70.0, 105.0)).into();
         let translation = Matrix4::from_translation(self.position);
 
@@ -129,7 +122,7 @@ impl Plane {
         );
 
         let scale_matrix = Matrix4::from_scale(self.scale);
-        let mut matrix = translation * plane_rot * scale_matrix;
+        let matrix = translation * plane_rot * scale_matrix;
         let camera_position = (matrix
             * Matrix4::from_translation(self.camera_offset)
             * Vector4::<f32>::new(0.0, 0.0, 0.0, 1.0))
@@ -138,7 +131,29 @@ impl Plane {
         camera.direction = self.forward;
         camera.up = self.up;
         camera.update_view();
-       // self.body.render(gl, matrix, time, camera);
+
+
+        let inconsolata = ab_glyph::FontArc::try_from_slice(include_bytes!(
+            "../assets/Inconsolata-Regular.ttf"
+        )).unwrap();
+
+        let mut glyph_brush = GlyphBrushBuilder::using_font(inconsolata).build(gl);
+
+        glyph_brush.queue(Section {
+            screen_position: (30.0, 90.0),
+            bounds: (800.0, 600.0),
+            text: vec![Text::default()
+                .with_text("Hello EFIS!")
+                .with_color([0.0, 0.0, 0.0, 1.0])
+                .with_scale(40.0)],
+            ..Section::default()
+        });
+
+        glyph_brush
+        .draw_queued(&gl, 800, 600)
+        .expect("Draw queued");
+
+        // self.body.render(gl, matrix, time, camera);
 
         // let offset = Matrix4::from_translation(Vector3::<f32>::new(0.0, -0.1935, 0.0));
         // let rev_offset = Matrix4::from_translation(Vector3::<f32>::new(0.0, 0.1935, 0.0));
